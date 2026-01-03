2c409d4f27f510e7302f006c0505a04931a77840
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-01-03 11:06:23 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,28 @@
+# string
+a = "uvesh"
+print(a)
+
+# int
+b = 7
+print(b)
+
+# float
+c = 9.99
+print(c)
+
+# list
+d = ["a","b","c","d",]
+print(d)
+
+# tuple
+e = ("a","b","c","d")
+print(e)
+
+# bool
+f = True
+print(f)
+
+# nonetype
+g = None
+print(g)
+
