b48bfad9f185677545293ba4563dbb7615a016f0
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-01-31 11:23:49 +0530

    Add files via upload

---
+++ if_elif.py
@@ -0,0 +1,20 @@
+per = float(input("Enter your percentage : "))
+
+if per >= 100: 
+    print("You will get grad A+")
+elif per >= 90:
+    print("You will get grad A")
+elif per >= 80:
+    print("You will get grad B+")
+elif per >= 70:
+    print("You will get grad B")
+elif per >= 60:
+    print("You will get grad C+")
+elif per >= 50:
+    print("You will get grad C")
+elif per >= 40:
+    print("You will get grad D+")
+elif per >= 33:
+    print("You will get grad D")
+else : 
+    print("Better luck next time")
